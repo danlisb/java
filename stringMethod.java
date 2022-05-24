68e7bdfa4389560dad607de7abb0004339443627
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-05-24 17:47:59 -0300

    Add files via upload

---
+++ stringMethod.java
@@ -0,0 +1,16 @@
+public class stringMethod {
+    public static void main(String[] args) {
+        String nome = "Dan";
+        
+        //boolean result = nome.equals("Dan");
+        //int result = nome.length();
+        //char result = nome.charAt(0);
+        //int result = nome.indexOf("a");
+        //boolean result = nome.isEmpty();
+        //String result = nome.toUpperCase();
+        //String result = nome.toLowerCase();
+        String result = nome.replace('a', 'o');
+
+        System.out.println(result);
+    }
+}
