2c91800f5b327f839b9c6b8d6a3cef9c4a318769
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-05-24 18:13:49 -0300

    Add files via upload

---
+++ wrapperClasses.java
@@ -0,0 +1,14 @@
+public class wrapperClasses {
+    public static void main(String[] args) {
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Dan";
+
+        if (b == '@') {
+            System.out.println("This is true");
+        }
+    }
+}
