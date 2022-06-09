1f5976704823a5650082771de36ab5e27d285921
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-06-09 18:53:04 -0300

    Create PrimitiveTypesChallenge.java

---
+++ Java Programming Masterclass udemy/PrimitiveTypesChallenge.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class PrimitiveTypesChallenge {
+
+    public static void main(String[] args) {
+	      byte myByte = 50;
+        short myShort = 35;
+        int myInt = 5;
+        long myLong = 50000L + (10 * (myByte + myShort + myInt));
+        System.out.println(myLong);
+    }
+}
