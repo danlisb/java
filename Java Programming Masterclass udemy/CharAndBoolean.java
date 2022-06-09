a0a3e2b94f5bf4ba365b62b782a9a1c2dd1e28a5
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-06-09 18:38:24 -0300

    Create CharAndBoolean.java

---
+++ Java Programming Masterclass udemy/CharAndBoolean.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class CharAndBoolean {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044'; //unicode table
+        char myCopyrightChar = '\u00A9'; //unicode table
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+
+        boolean isCustomerOverEighteen = true;
+    }
+}
