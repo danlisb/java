3cd45870f651efa4bada9ef530a1168725806667
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-08-02 23:08:34 -0300

    Update PrimitiveTypesChallenge.java

--- Java Programming Masterclass udemy/PrimitiveTypesChallenge.java
+++ Java Programming Masterclass udemy/PrimitiveTypesChallenge.java
@@ -3,7 +3,7 @@ package com.company;
 public class PrimitiveTypesChallenge {
 
     public static void main(String[] args) {
-	      byte myByte = 50;
+	byte myByte = 50;
         short myShort = 35;
         int myInt = 5;
         long myLong = 50000L + (10 * (myByte + myShort + myInt));
